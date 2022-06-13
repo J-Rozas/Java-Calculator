f2c4db2f0029ebe0cdfc81441495fd4394dfa596
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-13 19:24:14 +0100

    Add test for function add()

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -1,2 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(5, calculator.add(1, 4));
+    }
 }
