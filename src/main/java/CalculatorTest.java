5239f177669c42e54990bfd24a581b762ca34617
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-13 19:25:35 +0100

    Add test for function subtract()

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 
     Calculator calculator;
@@ -13,5 +15,10 @@ public class CalculatorTest {
     @Test
     public void canAdd() {
         assertEquals(5, calculator.add(1, 4));
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(7, calcualtor.subtract(10, 3));
     }
 }
